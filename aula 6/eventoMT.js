da1695487dfe9f334ac3774d64440b0cf1a5c1f6
Thyago <thyago.ezequiel.de.melo@gmail.com>
2023-11-09 12:37:57 -0300

    Atualização Aula 6

---
+++ aula 6/eventoMT.js
@@ -0,0 +1,28 @@
+document.addEventListener('contextmenu', function (event) {event.preventDefault();});
+/*
+document.addEventListener('mousedown', function (event) {console.log("Button: " + event.button);});
+*/
+
+/*
+document.addEventListener('mousedown', function (event) {console.log("Buttons: " + event.buttons);});
+*/
+
+/*
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 65) {
+        console.log('Tecla "A" foi pressionada.');
+    } else if (event.keyCode === 13) {
+        console.log('Tecla "Enter" foi pressionada.');
+    }
+});
+*/
+
+/*
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'a' && event.shiftKey) {
+        console.log('Tecla "A" foi pressionada.');
+    } else if (event.key === 'Enter') {
+        console.log('Tecla "Enter" foi pressionada.');
+    }
+});
+*/
